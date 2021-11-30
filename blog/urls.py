163cafd060e7ef37bd618a0051b4dcb9b51721e6
sumaiyamahin <snawarmahin@gmail.com>
2021-11-30 12:50:30 -0500

    commit 2. added serializer, viewset, drf

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,12 @@
-from django.urls import path
+from django.urls import include, path
 from . import views
+from rest_framework import routers
+from blog.views import PostViewSet
+
+router = routers.DefaultRouter()
+router.register(r'post', PostViewSet)
 
 urlpatterns = [
+    path('', include(router.urls)),
     path('', views.post_list, name='post_list'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from blog.serializers import PostSerializer
+from blog.models import Post
+
+class PostViewSet(viewsets.ModelViewSet):
+  queryset = Post.objects.all()
+  serializer_class = PostSerializer
+
 def post_list(request):
     return render(request, 'blog/post_list.html', {})
