b23b435c41ef562cc04bca600a8ecdbaa641248f
sumaiyamahin <snawarmahin@gmail.com>
2021-12-04 14:33:51 -0500

    urls & admin not working

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from .models import Post
+from .models import Books
 
 # Register your models here.
 admin.site.register(Post)
+admin.site.register(Books)
--- mysite/settings.py
+++ mysite/settings.py
@@ -76,10 +76,28 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'app_db',
+        'USER': 'sumiaya_m',
+        'PASSWORD': 'my_secret_password',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql',
+    #     'NAME': 'blog_post',
+    #     'USER': 'sumaiya',
+    #     'PASSWORD': '1234',
+    #     'HOST': 'localhost',
+    #     'PORT': '5432',
+    #  }
+
+    # postgres (another db superuser) password 1234
 }
 
 
